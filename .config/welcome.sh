c595875cbd344f6f4c21e4d1c5b3e180a3a955a0
speedie <71722170+speediegamer@users.noreply.github.com>
2022-03-06 03:41:34 +0100

    Add files via upload

---
+++ .config/welcome.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "            ____  _____ "
+echo "  ___ _ __ |  _ \| ____|"
+echo " / __| '_ \| | | |  _|  "
+echo " \__ \ |_) | |_| | |___ "
+echo " |___/ .__/|____/|_____|"
+echo "     |_|                "
+echo "Welcome to spDE!"
+echo
+echo "Thank you for downloading and installing spDE! Please make sure to report any bugs you find. Do this by creating an issue at https://github.com/speediegamer/spDE, emailing me, or DMing me on Discord (speedie#8260)"
+echo
+echo "Need support? Feel free to join my Discord server: https://ffdiscord.github.io"
+echo "Remember: To see a list of keybinds, press CTRL+ALT+H!"
